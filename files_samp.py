f1d2fadc2e3a604e126c3cf7b82b54c658a880c8
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-10-15 14:55:27 +0530

    Person label from raw text using entity spacy label

---
+++ files_samp.py
@@ -0,0 +1,3 @@
+file1 = open("sample.txt","r")
+text = file1.read()
+print(text)
---
+++ person_label_from_text.py
@@ -0,0 +1,33 @@
+# pip install spacy
+# python -m spacy download en_core_web_sm
+
+import spacy
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+nlp = spacy.load("en_core_web_sm")
+
+# Process whole documents
+file1 = open("sample.txt","r")
+text = file1.read()
+# print(text)
+doc = nlp(text)
+
+# Analyze syntax
+
+# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+# Find named entities, phrases and concepts
+
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
+
+
+print('Names:')
+# lets try printing on the names in from the samples text files
+for entity in doc.ents:
+        if(entity.label_ == "PERSON"):
+                print(entity.text)
+
+
+file1.close()
